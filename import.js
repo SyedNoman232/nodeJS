b4d97dba9420cd0a207b43af93b23bc05145c6a4
SyedNoman232 <sdnoman232@gmail.com>
2023-09-22 14:26:56 +0530

    import

---
+++ import.js
@@ -0,0 +1,13 @@
+//------------------NOW IMPORT FILE -------------------//
+// I WANT TO IMPORT DATA OF THIS FILE INTO ANOTHER FILE NAMED MAIN //
+
+module.exports = {
+     x : [1,2,3,4,5,6,7,8,9],
+
+     y : {Name:"syed",age:25,college:"life science"},
+
+     z : function(){
+        return "Hello World"
+     }
+};
+
